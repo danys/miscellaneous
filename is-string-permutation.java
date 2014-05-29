f58a4417b01434a4319710f15ec0dfce485df392
danys <netfox2006@hotmail.com>
2014-05-29 16:43:15 +0200

    Added second implementation O(N)

--- is-string-permutation.java
+++ is-string-permutation.java
@@ -17,12 +17,36 @@ public class permut
 		return (sorter(str1).compareTo(sorter(str2))==0);
 	}
 	
+	public static int[] charscanner(String str)
+	{
+		int count[] = new int[256]; //256 = size of ASCII character set
+		for(int i=0;i<count.length;i++) count[i]=0;
+		char c[] = str.toCharArray();
+		for(int i=0;i<str.length();i++)
+		{
+			count[c[i]-'a']++;
+		}
+		return count;
+	}
+	
+	public static boolean ispermutation2(String str1,String str2)
+	{
+		if (str1.length()!=str2.length()) return false;
+		int c1[] = charscanner(str1);
+		int c2[] = charscanner(str2);
+		for(int i=0;i<str1.length();i++)
+		{
+			if (c1[i]!=c2[i]) return false;
+		}
+		return true;
+	}
+	
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
 		String str1 = sc.nextLine();
 		String str2 = sc.nextLine();
 		sc.close();
-		System.out.println("String are pemut? = "+ispermutation(str1,str2));
+		System.out.println("String are pemut? = "+ispermutation2(str1,str2));
 	}
 }
